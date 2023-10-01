8ddcdb5012681938efd150d376736a6545e0331a
cristian588 <cdj.villalo@hotmail.com>
2023-10-01 12:39:39 -0500

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,63 @@
+using Application;
+
+using Infrastructure;
+
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+await app.Services.InitializeDatabasesAsync();
+
+app.UseInfrastructure(builder.Configuration);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Infrastructure/Auth/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Auth.Jwt;
+using Infrastructure.Auth.Permissions;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Auth
+{
+    internal static class Startup
+    {
+        internal static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration config)
+        {
+            return services
+                .AddPermissions()
+                .AddJwtAuth(config);
+        }
+
+        private static IServiceCollection AddPermissions(this IServiceCollection services) =>
+        services
+            .AddAuthorization(options =>
+            {
+                options.AddPolicy("BasicUserPermissionsPolicy", policy =>
+                {
+                    policy.Requirements.Add(new BasicUserPermissionsRequirement());
+                });
+                options.AddPolicy("AdminUserPermissionsPolicy", policy =>
+                {
+                    policy.Requirements.Add(new AdminUserPermissionsRequirement());
+                });
+            })
+            .AddScoped<IAuthorizationHandler, AdminUserPermissionsHandler>()
+            .AddScoped<IAuthorizationHandler, BasicUserPermissionsHandler>();
+    }
+}
